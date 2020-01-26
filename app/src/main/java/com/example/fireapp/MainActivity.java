2e110a9baa20c8dde3beeb752676d2d43f4bbada
ayahya24 <55040802+ayahya24@users.noreply.github.com>
2020-01-25 18:01:18 -0600

    Basic information display

--- app/src/main/java/com/example/fireapp/MainActivity.java
+++ app/src/main/java/com/example/fireapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.fireapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.inputconfirm);
+        final TextView txt = findViewById(R.id.textView2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt.setText("Kombucha time");
+            }
+        });
     }
+
 }
